0b85c5fe256509be2ed58ac126a8d1007ba20928
ItsTheGuy <36568572+ItsTheGuy@users.noreply.github.com>
2022-07-04 17:16:00 +0200

    Changed license banner

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # Singular
-# Copyright (C) 2021 ItsTheGuy
+# Copyright (C) 2022 Oscar
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
